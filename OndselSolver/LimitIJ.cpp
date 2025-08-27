bacb65f9c0deebe548f0b9729ecae5f394e059a6
Jacob Oursland <jacob.oursland@gmail.com>
2025-08-26 21:50:10 -0700

    Remove overzealous asserts that break debug builds by deferring to base class.

--- OndselSolver/LimitIJ.cpp
+++ OndselSolver/LimitIJ.cpp
@@ -3,22 +3,6 @@
 
 using namespace MbD;
 
-MbD::LimitIJ::LimitIJ()
-{
-}
-
-void MbD::LimitIJ::fillAccICIterError(FColDsptr col)
-{
-    (void) col;
-	assert(false);
-}
-
-void MbD::LimitIJ::fillAccICIterJacob(SpMatDsptr mat)
-{
-    (void) mat;
-	assert(false);
-}
-
 bool MbD::LimitIJ::satisfied() const
 {
 	auto& constraint = constraints->front();
@@ -49,24 +33,6 @@ void MbD::LimitIJ::fillConstraints(std::shared_ptr<std::vector<std::shared_ptr<C
 	}
 }
 
-void MbD::LimitIJ::fillDispConstraints(std::shared_ptr<std::vector<std::shared_ptr<Constraint>>> dispConstraints)
-{
-    (void) dispConstraints;
-	assert(false);
-}
-
-void MbD::LimitIJ::fillEssenConstraints(std::shared_ptr<std::vector<std::shared_ptr<Constraint>>> essenConstraints)
-{
-    (void) essenConstraints;
-	assert(false);
-}
-
-void MbD::LimitIJ::fillPerpenConstraints(std::shared_ptr<std::vector<std::shared_ptr<Constraint>>> perpenConstraints)
-{
-    (void) perpenConstraints;
-	assert(false);
-}
-
 void MbD::LimitIJ::fillPosICError(FColDsptr col)
 {
 	if (active) {
@@ -81,65 +47,11 @@ void MbD::LimitIJ::fillPosICJacob(SpMatDsptr mat)
 	}
 }
 
-void MbD::LimitIJ::fillPosKineError(FColDsptr col)
-{
-    (void) col;
-	assert(false);
-}
-
-void MbD::LimitIJ::fillPosKineJacob(SpMatDsptr mat)
-{
-    (void) mat;
-	assert(false);
-}
-
-void MbD::LimitIJ::fillqsuddotlam(FColDsptr col)
-{
-    (void) col;
-	assert(false);
-}
-
 void MbD::LimitIJ::fillqsulam(FColDsptr col)
 {
 	if (active) {
 		ConstraintSet::fillqsulam(col);
 	}
-}
-
-void MbD::LimitIJ::fillqsudot(FColDsptr col)
-{
-    (void) col;
-	assert(false);
-}
-
-void MbD::LimitIJ::fillqsudotWeights(DiagMatDsptr diagMat)
-{
-    (void) diagMat;
-	assert(false);
-}
-
-void MbD::LimitIJ::fillVelICError(FColDsptr col)
-{
-    (void) col;
-	assert(false);
-}
-
-void MbD::LimitIJ::fillVelICJacob(SpMatDsptr mat)
-{
-    (void) mat;
-	assert(false);
-}
-
-void MbD::LimitIJ::setqsuddotlam(FColDsptr col)
-{
-    (void) col;
-	assert(false);
-}
-
-void MbD::LimitIJ::setqsudotlam(FColDsptr col)
-{
-    (void) col;
-	assert(false);
 }
 
 void MbD::LimitIJ::setqsulam(FColDsptr col)
--- OndselSolver/LimitIJ.h
+++ OndselSolver/LimitIJ.h
@@ -15,25 +15,11 @@ namespace MbD {
 	{
 		//
 	public:
-		LimitIJ();
-		void fillAccICIterError(FColDsptr col) override;
-		void fillAccICIterJacob(SpMatDsptr mat) override;
+		LimitIJ() = default;
 		void fillConstraints(std::shared_ptr<std::vector<std::shared_ptr<Constraint>>> allConstraints) override;
-		void fillDispConstraints(std::shared_ptr<std::vector<std::shared_ptr<Constraint>>> dispConstraints) override;
-		void fillEssenConstraints(std::shared_ptr<std::vector<std::shared_ptr<Constraint>>> essenConstraints) override;
-		void fillPerpenConstraints(std::shared_ptr<std::vector<std::shared_ptr<Constraint>>> perpenConstraints) override;
 		void fillPosICError(FColDsptr col) override;
 		void fillPosICJacob(SpMatDsptr mat) override;
-		void fillPosKineError(FColDsptr col) override;
-		void fillPosKineJacob(SpMatDsptr mat) override;
-		void fillqsuddotlam(FColDsptr col) override;
 		void fillqsulam(FColDsptr col) override;
-		void fillqsudot(FColDsptr col) override;
-		void fillqsudotWeights(DiagMatDsptr diagMat) override;
-		void fillVelICError(FColDsptr col) override;
-		void fillVelICJacob(SpMatDsptr mat) override;
-		void setqsuddotlam(FColDsptr col) override;
-		void setqsudotlam(FColDsptr col) override;
 		void setqsulam(FColDsptr col) override;
 		void useEquationNumbers() override;
 
